38b9efbaff38ba1cc8a6e63f0b8a7eb22336b38a
Andy Agus <redagus@gmail.com>
2020-05-25 01:17:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,5 +17,3 @@ def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   planeteer_calls.find{|num| valid_calls.include?(num)}
 end
-
-planeteer_calls.find{|num| valid_calls.include?(num)}
